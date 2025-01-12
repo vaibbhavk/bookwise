b04b1b1392b4f8ac99524605b7a193ba7afb4db2
vaibbhavk <vaibhav.vk2128@gmail.com>
2025-01-12 18:48:00 +0530

    Book cover section

---
+++ components/BookCover.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const variantStyles = {
+    extraSmall
+}
+
+interface Props {
+    className?: string
+    variant: "wide" | "small"
+    coverColor: string
+
+}
+
+const BookCover = ({className, variant, coverColor, coverImage}: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BookCover
